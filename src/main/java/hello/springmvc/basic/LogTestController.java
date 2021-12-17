2cd8c1b64776bdc1a82f2cd80d4d7a2d6d8e9ef9
JungTag <dbsdudxor121@naver.com>
2021-12-17 14:55:12 +0900

    feat: 로깅 설정

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController // 뷰 리졸버 X, HTTP 메세지 바디에 바로 입
+public class LogTestController {
+
+//    @Slf4j 어노테이션 쓰면 아래 문장 안 써도 됨!
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
